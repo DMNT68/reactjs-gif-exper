560fbe47038c410d17e2e5ffe892007689a744a2
Andrés Salgado <andressalgado41@gmail.com>
2021-05-05 23:03:01 -0500

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+  //   const categories = ['One Punchman', 'Samurai X', 'Dragon ball'];
+  const [categories, setCategories] = useState(['One puchman']);
+
+  return (
+    <div>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category, idx) => (
+          <GifGrid key={`${(category = idx)}`} category={category} />
+        ))}
+      </ol>
+    </div>
+  );
+};
