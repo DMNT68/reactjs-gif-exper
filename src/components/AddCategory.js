aceb9da2a4fe949289f279df0aab12a82f351cc9
Andrés Salgado <andressalgado41@gmail.com>
2021-05-07 22:23:42 -0500

    test en AddCategory

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -18,6 +18,7 @@ export const AddCategory = ({ setCategories }) => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <p>{inputValue}</p>
       <input type="text" value={inputValue} placeholder="Escriba un nombre" onChange={hadleInputChange} />
     </form>
   );
---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import { shallow } from "enzyme";
+import { AddCategory } from "../../components/AddCategory";
+
+describe('Pruebas en <AddCategory/>', () => {
+   
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+    beforeEach(()=>{
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    });
+
+    test('debe mostrarse correctamente', () => {
+       expect(wrapper).toMatchSnapshot(); 
+    });
+
+    test('debe de cambiar la caja de texto', () => {
+       const input = wrapper.find('input');
+       const value = 'hola mundo';
+       input.simulate('change', { target:{value}}); 
+       
+       expect(wrapper.find('p').text().trim()).toBe(value);
+    });
+
+    test('No debe de postear la información con submit', () => {
+        wrapper.find('form').simulate('submit', {preventDefault(){}});
+        expect(setCategories).not.toHaveBeenCalled()
+    });
+    
+    
+
+});
